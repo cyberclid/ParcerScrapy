492415547c5a69c9fe7ebf165020c1fd9418b8d3
lilian <lilian.lungu.77@mail.ru>
2023-03-01 22:23:39 +0200

    without method request

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -4,14 +4,11 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
+    urls = [
+        'https://quotes.toscrape.com/page/1/',
+        'https://quotes.toscrape.com/page/2/',
+    ]
 
-    def start_requests(self):
-        urls = [
-            'https://quotes.toscrape.com/page/1/',
-            'https://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
         page = response.url.split("/")[-2]
